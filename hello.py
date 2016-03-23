675b47f2ef2a2070031a57b9d1452cdff9b647ab
Heier <zouyong656168634@outlook.com>
2016-03-23 19:57:50 +0800

    测试学习python-2016/03/23

--- hello.py
+++ hello.py
@@ -1,1 +1,33 @@
-print( 'Hello World' );
+# name=input( '请输入你的名字:' )
+# print( 'Hello,', name )
+# print( '\"i\'m ok\"' )
+# print( r'\\\t\\' )
+
+# name = '''
+# 这是一个测试\n\t 1
+# 好玩的测试
+# i'm ok
+# '''
+# print( name )
+
+# print( 3 > 2 )
+# print( True and False )
+
+# a = 'ABC'
+# b = a
+# a = 'XYZ'
+# print(b)
+
+# print( 10.3 // 3.1 )
+
+# print( 'ABC'.encode( 'ascii' ) )
+# print( '中文'.encode( 'utf-8' ) )
+# print( 'ABC'.encode( 'ascii' ) )
+
+# print( 'Hello %s' % 'heier' )
+# print( 'Hello %s, you money is %04d' % ( 'heier', 100 ) )
+# print( 'Hello %s, you money is %04d%%' % ( 'heier', 100 ) )
+
+days = ['one', 'two', 'three']
+print( days )
+len( days )
